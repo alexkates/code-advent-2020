2cd6f7b66a524a1866e76bd7a95bd7deccb94001
Daniel Alexander Kates <alex.kates@creditly.com>
2020-12-01 14:21:25 -0500

    added editorconfig

--- 1/index.js
+++ 1/index.js
@@ -1,23 +1,23 @@
 const fs = require('fs');
 
 const main = async () => {
-    const rawData = fs.readFileSync('./input', 'utf-8');
-    const data = rawData.split('\n');
+  const rawData = fs.readFileSync('./input', 'utf-8');
+  const data = rawData.split('\n');
 
-    for (let i = 0; i < data.length; i++) {
-        for (let j = 0; j < data.length; j++) {
-            for (let k = 0; k < data.length; k++) {
-                const eye = parseInt(data[i]);
-                const jay = parseInt(data[j]);
-                const kay = parseInt(data[k]);
+  for (let i = 0; i < data.length; i++) {
+    for (let j = 0; j < data.length; j++) {
+      for (let k = 0; k < data.length; k++) {
+        const eye = parseInt(data[i]);
+        const jay = parseInt(data[j]);
+        const kay = parseInt(data[k]);
 
-                if (eye + jay + kay === 2020) {
-                    console.log(eye * jay * kay);
-                    return;
-                }
-            }
+        if (eye + jay + kay === 2020) {
+          console.log(eye * jay * kay);
+          return;
         }
+      }
     }
+  }
 };
 
 main();
